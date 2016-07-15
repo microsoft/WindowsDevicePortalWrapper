0b74bf7ed066154025607bbbba8c18fd68524b9d
Matt Hyman <mathy@microsoft.com>
2016-07-15 12:42:27 -0700

    Add web socket support for UWP

---
+++ WindowsDevicePortalWrapper/UnitTestProject/WDPMockImplementations/WebSocket.cs
@@ -0,0 +1,50 @@
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="WebSocket.cs" company="Microsoft Corporation">
+//     Licensed under the MIT License. See LICENSE.TXT in the project root license information.
+// </copyright>
+//----------------------------------------------------------------------------------------------
+
+using System;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Microsoft.Tools.WindowsDevicePortal
+{
+    /// <summary>
+    /// MOCK implementation of HTTP web socket wrapper
+    /// </summary>
+    /// <typeparam name="T">Return type for the websocket messages.</typeparam>
+    internal partial class WebSocket<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebSocket{T}" /> class.
+        /// </summary>
+        /// <param name="connection">Implementation of a connection object.</param>
+        /// <param name="serverCertificateValidationHandler">Server certificate handler.</param>
+        public WebSocket(IDevicePortalConnection connection, Func<object, X509Certificate, X509Chain, SslPolicyErrors, bool> serverCertificateValidationHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stops listneing for messages from the websocket and closes the connection to the websocket.
+        /// </summary>
+        /// <returns>The task of closing the websocket connection.</returns>
+        private async Task StopListeningForMessagesInternal()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Connects to the websocket and starts listening for messages from the websocket.
+        /// Once they are received they are parsed and the WebSocketMessageReceived event is raised.
+        /// </summary>
+        /// <param name="endpoint">The uri that the weboscket should connect to</param>
+        /// <returns>The task of listening for messages from the websocket.</returns>
+        private async Task StartListeningForMessagesInternal(Uri endpoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.UniversalWindows/HttpRest/WebSocket.cs
@@ -0,0 +1,137 @@
+﻿//----------------------------------------------------------------------------------------------
+// <copyright file="WebSocket.cs" company="Microsoft Corporation">
+//     Licensed under the MIT License. See LICENSE.TXT in the project root license information.
+// </copyright>
+//----------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Security.Credentials;
+using Windows.Storage.Streams;
+
+namespace Microsoft.Tools.WindowsDevicePortal
+{
+    /// <summary>
+    /// HTTP Websocket Wrapper
+    /// </summary>
+    /// <typeparam name="T">Return type for the websocket messages.</typeparam>
+    internal partial class WebSocket<T>
+    {
+        /// <summary>
+        /// The <see cref="MessageWebSocket" /> that is being wrapped.
+        /// </summary>
+        private MessageWebSocket websocket = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebSocket{T}" /> class.
+        /// </summary>
+        /// <param name="connection">Implementation of a connection object.</param>
+        public WebSocket(IDevicePortalConnection connection)
+        {
+            this.deviceConnection = connection;
+            this.IsListeningForMessages = false;
+        }
+
+        /// <summary>
+        /// Opens a connection to the specified websocket API and starts listening for messages.
+        /// </summary>
+        /// <param name="endpoint">The uri that the weboscket should connect to</param>
+        /// <returns>The task of opening a connection to the websocket.</returns>
+        private async Task StartListeningForMessagesInternal(
+            Uri endpoint)
+        {
+            this.websocket = new MessageWebSocket();
+
+            this.websocket.Control.MessageType = SocketMessageType.Utf8;
+
+            this.websocket.MessageReceived += this.MessageReceived;
+
+            this.websocket.Closed += (senderSocket, args) =>
+            {
+                if (this.websocket != null)
+                {
+                    this.websocket.Dispose();
+                    this.websocket = null;
+                    this.IsListeningForMessages = false;
+                }
+            };
+
+            PasswordCredential cred = new PasswordCredential();
+            cred.UserName = this.deviceConnection.Credentials.UserName;
+            cred.Password = this.deviceConnection.Credentials.Password;
+
+            this.websocket.Control.ServerCredential = cred;
+            this.websocket.SetRequestHeader("Origin", this.deviceConnection.Connection.AbsoluteUri);
+
+            // Do not wait on receiving messages.
+            Task connectTask = this.ConnectAsync(endpoint);
+        }
+
+        /// <summary>
+        /// Opens a connection to the specified websocket API.
+        /// </summary>
+        /// <param name="endpoint">The uri that the weboscket should connect to</param>
+        /// <returns>The task of opening a connection to the websocket.</returns>
+        private async Task ConnectAsync(
+            Uri endpoint)
+        {
+            bool connecting = true;
+            try
+            {
+                await this.websocket.ConnectAsync(endpoint);
+                connecting = false;
+                this.IsListeningForMessages = true;
+            }
+            catch (Exception)
+            {
+                // Error happened during connect operation.
+                if (connecting && this.websocket != null)
+                {
+                    this.websocket.Dispose();
+                    this.websocket = null;
+                    this.IsListeningForMessages = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts received stream to a parsed message and passes it to the WebSocketMessageReceived handler.
+        /// </summary>
+        /// <param name="sender">The  <see cref="MessageWebSocket" /> that sent the message.</param>
+        /// <param name="args">The message from the web socket.</param>
+        private void MessageReceived(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
+        {
+            using (IInputStream inputStream = args.GetDataStream())
+            {
+                Stream stream = new MemoryStream();
+
+                Task copyTask = inputStream.AsStreamForRead().CopyToAsync(stream);
+                copyTask.Wait();
+
+                // Ensure we return with the stream pointed at the origin.
+                stream.Position = 0;
+
+                this.ConvertStreamToMessage(stream);
+            }
+        }
+
+        /// <summary>
+        /// Closes the connection to the websocket.
+        /// </summary>
+        /// <returns>The task of closing the websocket connection.</returns>
+        private async Task StopListeningForMessagesInternal()
+        {
+            if (this.IsListeningForMessages)
+            {
+                if (this.websocket != null)
+                {
+                    this.websocket.Close(1000, "Closed due to user request.");
+                    this.websocket = null;
+                    this.IsListeningForMessages = false;
+                }
+            }
+        }
+    }
+}
