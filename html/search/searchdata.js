2debe5dd5dc5d9c59b2ff9b1b155fba3027d3c67
Jason Williams <jaswill@microsoft.com>
2016-08-08 14:38:22 -0700

    Update code documentation via UpdateDocs.cmd script

---
+++ html/search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghiklmnopqrstuvwx",
+  1: "abcdfghiklmnoprstuwx",
+  2: "msx",
+  3: "abcdeghilmoprstuvwx",
+  4: "abcdfghiklmoprstuvwx",
+  5: "adht",
+  6: "acdfghimnpruwx",
+  7: "abcdefgilmnopqrstuvwx",
+  8: "acrs",
+  9: "cruw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "properties",
+  8: "events",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Properties",
+  8: "Events",
+  9: "Pages"
+};
+
---
+++ updateDocs.cmd
@@ -0,0 +1,20 @@
+ECHO OFF
+
+if "%1" equ "" ( 
+echo Must provide master repo path
+exit /b
+)
+
+del *.html
+del *.png
+del *.css
+del *.js
+rmdir /S /Q search
+
+pushd %1
+doxygen docconfig.txt
+popd
+move %1\html .
+git add *
+git commit -m "Update code documentation via UpdateDocs.cmd script"
+git push
