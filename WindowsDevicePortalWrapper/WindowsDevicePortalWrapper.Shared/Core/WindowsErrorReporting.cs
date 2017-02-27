4fd8709b083148412e2a81adf765df4168e1c136
Hirsch Singhal <hps.scout@gmail.com>
2017-02-27 12:06:59 -0800

    Missing using in WER

--- WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/Core/WindowsErrorReporting.cs
+++ WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/Core/WindowsErrorReporting.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Tools.WindowsDevicePortal
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Runtime.Serialization;
     using System.Threading.Tasks;
 
