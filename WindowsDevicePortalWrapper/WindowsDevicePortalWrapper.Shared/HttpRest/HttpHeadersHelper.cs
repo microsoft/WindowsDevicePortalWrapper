a105f2641bbb44484868c10a82e8338128fb0bf6
Jason Williams <jaswill@microsoft.com>
2017-04-20 13:12:43 -0700

    Restores our CSRF token handling (#238)
    Addresses Issue #216

--- WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/HttpRest/HttpHeadersHelper.cs
+++ WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/HttpRest/HttpHeadersHelper.cs
@@ -32,6 +32,11 @@ namespace Microsoft.Tools.WindowsDevicePortal
         private static readonly string ContentTypeHeaderName = "Content-Type";
 
         /// <summary>
+        /// Header name for a CSRF-Token.
+        /// </summary>
+        private static readonly string CsrfTokenName = "CSRF-Token";
+
+        /// <summary>
         /// Header name for a User-Agent.
         /// </summary>
         private static readonly string UserAgentName = "User-Agent";
@@ -40,6 +45,39 @@ namespace Microsoft.Tools.WindowsDevicePortal
         /// Header value for User-Agent for the WDPW Open Source project.
         /// </summary>
         private static readonly string UserAgentValue = "WindowsDevicePortalWrapper";
+
+        /// <summary>
+        /// CSRF token retrieved by GET calls and used on subsequent POST/DELETE/PUT calls.
+        /// This token is intended to prevent a security vulnerability from cross site forgery.
+        /// </summary>
+        private string csrfToken = string.Empty;
+
+        /// <summary>
+        /// Applies the CSRF token to the HTTP client.
+        /// </summary>
+        /// <param name="client">The HTTP client on which to have the header set.</param>
+        /// <param name="method">The HTTP method (ex: POST) that will be called on the client.</param>
+        private void ApplyCSRFHeader(
+            HttpClient client,
+            HttpMethods method)
+        {
+            string headerName = "X-" + CsrfTokenName;
+            string headerValue = this.csrfToken;
+
+            if (string.Compare(method.ToString(), "get", true) == 0)
+            {
+                headerName = CsrfTokenName;
+                headerValue = string.IsNullOrEmpty(this.csrfToken) ? "Fetch" : headerValue;
+            }
+
+#if WINDOWS_UWP
+            HttpRequestHeaderCollection headers = client.DefaultRequestHeaders;
+#else
+            HttpRequestHeaders headers = client.DefaultRequestHeaders;
+#endif // WINDOWS_UWP
+
+            headers.Add(headerName, headerValue);
+        }
 
         /// <summary>
         /// Applies any needed headers to the HTTP client.
@@ -51,6 +89,7 @@ namespace Microsoft.Tools.WindowsDevicePortal
             HttpMethods method)
         {
             this.ApplyUserAgentHeader(client);
+            this.ApplyCSRFHeader(client, method);
         }
 
         /// <summary>
@@ -75,6 +114,39 @@ namespace Microsoft.Tools.WindowsDevicePortal
 #endif // WINDOWS_UWP
 
             headers.Add(UserAgentName, userAgentValue);
+        }
+
+        /// <summary>
+        /// Retrieves the CSRF token from the HTTP response and stores it.
+        /// </summary>
+        /// <param name="response">The HTTP response from which to retrieve the header.</param>
+        private void RetrieveCsrfToken(HttpResponseMessage response)
+        {
+            // If the response sets a CSRF token, store that for future requests.
+#if WINDOWS_UWP
+            string cookie;
+            if (response.Headers.TryGetValue("Set-Cookie", out cookie))
+            {
+                string csrfTokenNameWithEquals = CsrfTokenName + "=";
+                if (cookie.StartsWith(csrfTokenNameWithEquals))
+                {
+                    this.csrfToken = cookie.Substring(csrfTokenNameWithEquals.Length);
+                }
+            }
+#else
+            IEnumerable<string> cookies;
+            if (response.Headers.TryGetValues("Set-Cookie", out cookies))
+            {
+                foreach (string cookie in cookies)
+                {
+                    string csrfTokenNameWithEquals = CsrfTokenName + "=";
+                    if (cookie.StartsWith(csrfTokenNameWithEquals))
+                    {
+                        this.csrfToken = cookie.Substring(csrfTokenNameWithEquals.Length);
+                    }
+                }
+            }
+#endif
         }
     }
 }
