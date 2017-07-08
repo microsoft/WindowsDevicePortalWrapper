8080597b2ded6238d42f161203c3df6517d7203e
Matt Hyman <mathy@microsoft.com>
2017-07-07 18:47:21 -0700

    Consolidate the assert on a deserialization failure to all rest requests and reduce duplicated logic.

--- WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/HttpRest/RestDelete.cs
+++ WindowsDevicePortalWrapper/WindowsDevicePortalWrapper.Shared/HttpRest/RestDelete.cs
@@ -42,28 +42,12 @@ namespace Microsoft.Tools.WindowsDevicePortal
             string apiPath,
             string payload = null) where T : new()
         {
-            T data = default(T);
-
             Uri uri = Utilities.BuildEndpoint(
                 this.deviceConnection.Connection,
                 apiPath, 
                 payload);
 
-            DataContractJsonSerializer deserializer = new DataContractJsonSerializer(typeof(T));
-
-            using (Stream dataStream = await this.DeleteAsync(uri))
-            {
-                if ((dataStream != null) &&
-                    (dataStream.Length != 0))
-                {
-                    JsonFormatCheck<T>(dataStream);
-
-                    object response = deserializer.ReadObject(dataStream);
-                    data = (T)response;
-                }
-            }
-
-            return data;
+            return ReadJsonStream<T>(await this.DeleteAsync(uri));
         }
     }
 }
